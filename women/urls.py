38d2b9ceeb012d260f68f926ddf6b158a1a31c1e
Nursultan301 <abykeevnursultan14@gmail.com>
2024-01-02 22:51:04 +0600

    fead: add detail.html

--- women/urls.py
+++ women/urls.py
@@ -4,5 +4,6 @@ from women import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:pk>', views.detail, name='detail'),
     path('about/', views.about, name='about'),
 ]
--- women/views.py
+++ women/views.py
@@ -1,11 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+db_data = [
+    {"id": 1, "title": "Новость1", "is_published": True},
+    {"id": 2, "title": "Новость2", "is_published": True},
+    {"id": 3, "title": "Новость3", "is_published": False},
+    {"id": 4, "title": "Новость4", "is_published": True}
+]
 
 
 def index(request):
     context = {
         "title": "Главная страница",
+        "posts": db_data
     }
     return render(request, 'women/index.html', context=context)
+
+
+def detail(request, pk):
+    return HttpResponse(f'Отображение статьи с id = {pk}')
 
 
 def about(request):
