b968e1d98f05aff18bb6504a0af5cd5383ea46dc
Nursultan301 <abykeevnursultan14@gmail.com>
2024-01-06 23:14:19 +0600

    fead: added the Category class and linked it with the Women class

--- women/models.py
+++ women/models.py
@@ -18,6 +18,7 @@ class Women(models.Model):
     time_created = models.DateTimeField(auto_now_add=True)
     time_updated = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     objects = models.Manager()
     published = PublishedManager()
@@ -36,3 +37,16 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'slug': self.slug})
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title', )
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
