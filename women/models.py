283b55971f85a80b843b9543defec6544180bf95
Nursultan301 <abykeevnursultan14@gmail.com>
2024-01-16 20:43:57 +0600

    fix: changed the path of the translit_to_eng function

--- women/models.py
+++ women/models.py
@@ -2,14 +2,7 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
 
-
-def translit_to_eng(s: str) -> str:
-    d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
-         'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'к': 'k',
-         'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
-         'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
-         'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'y', 'ъ': '', 'э': 'r', 'ю': 'yu', 'я': 'ya'}
-    return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
+from common.translit import translit_to_eng
 
 
 class PublishedManager(models.Manager):
